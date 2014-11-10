3267e362482cf6960eac7142df08cc33d926f844
August <AugtheMog@yahoo.com>
2014-11-10 10:38:49 -0500

    Added our pojo getter and setter methods etc for teams

--- app/src/main/java/com/aa/sportstree/sportstree/pojos/Team.java
+++ app/src/main/java/com/aa/sportstree/sportstree/pojos/Team.java
@@ -4,4 +4,36 @@ package com.aa.sportstree.sportstree.pojos;
  * Created by Abhinav on 11/9/2014.
  */
 public class Team {
+    private Sport sport;
+    private String teamName;
+    private String teamId; // every team has an id
+
+    public Team(Sport sport, String teamName, String teamId) {
+        this.sport = sport;
+        this.teamName = teamName;
+        this.teamId = teamId;
+    }
+
+    public Sport getSport() {
+        return sport;
+    }
+
+    public void setSport(Sport sport) {
+        this.sport = sport;
+    }
+    public String getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(String teamId) {
+        this.teamId = teamId;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
 }
