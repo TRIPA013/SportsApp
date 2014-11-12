1af04282bd59eeb89b94e3b98a876548ac2299e6
August <AugtheMog@yahoo.com>
2014-11-12 17:08:00 -0500

    Updating file reading to include the inputstream, cannot use the file like we thought we could

--- app/src/main/java/com/aa/sportstree/sportstree/util/FileUtil.java
+++ app/src/main/java/com/aa/sportstree/sportstree/util/FileUtil.java
@@ -5,7 +5,9 @@ import com.aa.sportstree.sportstree.pojos.Team;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -13,19 +15,17 @@ import java.util.Scanner;
  * Created by Abhinav on 11/10/2014.
  */
 public class FileUtil {
-    public static List<String> getTeams(File file) {
+    public static List<String> getTeams(InputStream input) {
         List<String> teams = new ArrayList<String>();
         try {
-            Scanner scanner = new Scanner(file);
+            Scanner scanner = new Scanner(input);
             while (scanner.hasNext()) {
                 teams.add(scanner.nextLine());
 
             }
-        } catch (FileNotFoundException e) {
+        } catch (InputMismatchException e) {
             System.out.println("Didn't find " + e);
         }
-
-
         return teams;
     }
 }
