5739869eaa3c4af0b91cc19c638d893189fbf704
TRIPA013 <abhinav.tripathi@disney.com>
2014-11-14 16:11:47 -0500

    Adding volley library to the project.

--- app/src/main/java/com/aa/sportstree/sportstree/util/VolleyUtil.java
+++ app/src/main/java/com/aa/sportstree/sportstree/util/VolleyUtil.java
@@ -1,11 +1,15 @@
 package com.aa.sportstree.sportstree.util;
 
+import com.android.volley.RequestQueue;
+
 /**
  * Created by Abhinav on 11/14/2014.
  */
 public class VolleyUtil {
 
 
-    //RequestQueue requestQueue;
+
+
+    RequestQueue requestQueue;
 
 }
